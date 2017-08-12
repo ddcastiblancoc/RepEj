eb9bcb723590558c4e8f8c958bb57de1498d35e0
Daniel Castiblanco <ddcastiblancoc@unal.edu.co>
2017-08-11 23:02:37 -0700

    2

---
+++ Ejercicio2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(void)
+{
+  double x, y;
+  std::cout<<"Escriba la cantidad a convertir, en furlongs"<<std::endl;
+  std::cin>>x;
+  y=x/220;
+  std::cout<<"La cantidad en yardas es "<<y<<" yd"<<std::endl;
+  return 0;
+}
+  
